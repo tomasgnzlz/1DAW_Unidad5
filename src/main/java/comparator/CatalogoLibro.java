919a227515cc87c73d92b291f8ecf595410f231e
tgonati327 <tgonati327@g.educaand.es>
2024-01-11 12:45:47 +0100

    -

--- src/main/java/comparator/CatalogoLibro.java
+++ src/main/java/comparator/CatalogoLibro.java
@@ -4,6 +4,11 @@
  */
 package comparator;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  *
  * @author tomas
@@ -20,6 +25,28 @@ public class CatalogoLibro {
         System.out.println(l2.hashCode());
         
         Libro l3 = new Libro("arvnuirv7", "VicoVico", "Vico", "bbbb", 20);
+        ArrayList<Libro> lista = new ArrayList<>();
+        lista.add(new Libro("1234", "titulo", "autor", "editorial", 10));
+        lista.add(new Libro("321", "titulo2", "autor2", "editorial2", 546));
+        lista.add(new Libro("023", "titulo3", "autor3", "editorial3", 01));
+        System.out.println("\n*****************LISTA ORDENADA*****************\n");
+        lista.forEach(System.out::println);
+        
+        // ordenar por isbn
+        //Comparator<Libro>
+                
+        Collections.sort(lista, (e1,e2)->e1.getIsbn().compareToIgnoreCase(e2.getIsbn()));
+        System.out.println("\n*****************LISTA ORDENADA*****************\n");
+        lista.forEach(System.out::println);
+        
+        // por numero de pagina
+        System.out.println("\n*****************LISTA ORDENADA POR NUMPAGINAS*****************\n");
+        Collections.sort(lista, (k1,k2)->Integer.compare(k1.getNumPaginas(), k2.getNumPaginas()));
+        lista.forEach(System.out::println);
+        
+        
     }
     
 }
+
+
